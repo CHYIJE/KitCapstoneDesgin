9073017ba9f8df2c429ef638b7f6c728d394b73c
d0gn <118961967+d0gn@users.noreply.github.com>
2023-12-08 15:56:13 +0900

    0.01

---
+++ Assets/Sciprts/shareInt.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shareInt : MonoBehaviour
+{
+    public int userProgressInt = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
